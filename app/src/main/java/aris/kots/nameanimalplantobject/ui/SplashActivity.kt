0d8b827f954abf0c2a50605988cc1e1c125de136
Aris Kots <ArisKots1992@gmail.com>
2018-10-01 00:24:09 +0300

    constraint layout animations

--- app/src/main/java/aris/kots/nameanimalplantobject/ui/SplashActivity.kt
+++ app/src/main/java/aris/kots/nameanimalplantobject/ui/SplashActivity.kt
@@ -15,7 +15,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        lol.text="Aristotelis"
+        lol.text="Άρης"
 
 
         val intent = Intent(this, MainActivity::class.java)
